29703c5eee6c62c1d99ff90bd80f375c9cd9b683
Josh Marchello <jmarchello@mersoft.com>
2018-08-01 18:21:08 -0500

    added a try catch in case directory does not exist

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const fs = require('fs');
 
 const importFromDirectory = (directoryPath, maxPasses=4) => {
-    const files = fs.readdirSync(directoryPath).filter(file => file.endsWith('.js'));
-    const result = {};
-    let passes = 0;
-    let errors = [];
+    try {
+        const files = fs.readdirSync(directoryPath).filter(file => file.endsWith('.js'));
+        const result = {};
+        let passes = 0;
+        let errors = [];
 
-    while (files.length > 0 && passes <= maxPasses) {
-        files.map(file => {
-            try {
-                result[file.split('.')[0]] = require(`${directoryPath}/${file}`);
-                files.splice(files.indexOf(file), 1);
-            } catch (err) {
-                errors.push(err);
-            }
-        });
-        passes++;
+        while (files.length > 0 && passes <= maxPasses) {
+            files.map(file => {
+                try {
+                    result[file.split('.')[0]] = require(`${directoryPath}/${file}`);
+                    files.splice(files.indexOf(file), 1);
+                } catch (err) {
+                    errors.push(err);
+                }
+            });
+            passes++;
+        }
+
+        if (passes >= maxPasses && files.length > 0) {
+            throw new Error(`Maximum import attempts reached, the following errors occured on the last attempt: ${errors}`);
+        }
+
+        return result;
+    } catch (err) {
+        console.log(err);
     }
-
-    if (passes >= maxPasses && files.length > 0) {
-        throw new Error(`Maximum import attempts reached, the following errors occured on the last attempt: ${errors}`);
-    }
-
-    return result;
 };
 
 module.exports = importFromDirectory;
